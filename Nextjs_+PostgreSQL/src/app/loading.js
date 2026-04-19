52a21484cdcd9b9164467bc3aa2cd6cd4984a0ad
Farrukh Khalid <khalidfarrukh951@gmail.com>
2026-04-19 19:17:26 +0500

    added loading.js file on home as well

---
+++ Nextjs_+PostgreSQL/src/app/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center bg-background_1">
+      <div className="text-lg font-medium animate-pulse">
+        Loading product...
+      </div>
+    </div>
+  );
+}
