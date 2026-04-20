6d763ab6317982ee9bb6a6b659749abf8dfe089c
Farrukh Khalid <khalidfarrukh951@gmail.com>
2026-04-20 20:46:44 +0500

    fixed cart loading issue when cart is empty

--- Nextjs_+PostgreSQL/src/app/loading.js
+++ Nextjs_+PostgreSQL/src/app/loading.js
@@ -2,6 +2,8 @@ import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function Loading() {
   return (
-    <LoadingSpinner text="Loading products" />
+    <div className="min-h-[calc(100vh-60px-98px-176px)] md:min-h-[calc(100vh-60px-98px-140px)] flex items-center justify-center">
+      <LoadingSpinner text="Loading products" />
+    </div>
   );
 }
