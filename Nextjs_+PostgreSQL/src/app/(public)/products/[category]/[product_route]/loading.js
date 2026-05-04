10b89fdf3cb5a9e4e7c4326f6ad3d4a3066b115d
Farrukh Khalid <khalidfarrukh951@gmail.com>
2026-05-04 12:21:43 +0500

    removed loading file from products

--- Nextjs_+PostgreSQL/src/app/(public)/products/[category]/[product_route]/loading.js
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
-
-export default function Loading() {
-  return (
-    <div className="min-h-[calc(100vh-60px-98px-176px)] md:min-h-[calc(100vh-60px-98px-140px)] flex items-center justify-center">
-      <LoadingSpinner text="Loading" />
-    </div>
-  );
-}
